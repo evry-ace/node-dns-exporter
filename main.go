7be6d57c6be8f846a6e4640d70648a802cad28cc
Hans Kristian Flaatten <hans.flaatten@evry.com>
2021-02-27 11:54:27 +0100

    Add support for custom host test

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"strings"
 
 	"github.com/docker/libnetwork/resolvconf"
 	"github.com/docker/libnetwork/types"
@@ -13,36 +17,57 @@ import (
 var (
 	promNameserver = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "nameserver",
+			Namespace: "node_dns",
+			Name:      "nameserver",
 		},
 		[]string{"server"},
 	)
 	promSearchdomain = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "searchdomain",
+			Namespace: "node_dns",
+			Name:      "searchdomain",
 		},
-		[]string{"domain"},
+		[]string{"host"},
+	)
+	promHostTest = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "node_dns",
+			Name:      "test_result",
+		},
+		[]string{"host", "result", "status"},
 	)
 )
+
+var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
+var test = flag.String("test-hosts", "", "Comma separated list of hosts to test DNS resolution")
 
 func init() {
 	prometheus.MustRegister(promNameserver)
 	prometheus.MustRegister(promSearchdomain)
+	prometheus.MustRegister(promHostTest)
 }
 
 func main() {
-	//fmt.Println(net.LookupHost("example.com"))
+	flag.Parse()
 
 	conf, _ := resolvconf.Get()
 	nameservers := resolvconf.GetNameservers(conf.Content, types.IPv4)
 	searchdomains := resolvconf.GetSearchDomains(conf.Content)
+	testhosts := strings.Split(*test, ",")
 
 	for _, server := range nameservers {
 		promNameserver.With(prometheus.Labels{"server": server}).Inc()
 	}
 
-	for _, domain := range searchdomains {
-		promSearchdomain.With(prometheus.Labels{"domain": domain}).Inc()
+	for _, host := range searchdomains {
+		promSearchdomain.With(prometheus.Labels{"host": host}).Inc()
+	}
+
+	for _, host := range testhosts {
+		result, _ := net.LookupHost(host)
+		fmt.Printf("Resolved %s to %s\n", host, result)
+		promHostTest.With(prometheus.Labels{"host": host, "status": "success", "result": strings.Join(result, ",")}).Set(1)
+		promHostTest.With(prometheus.Labels{"host": host, "status": "failed", "result": ""}).Set(0)
 	}
 
 	http.Handle("/metrics", promhttp.Handler())
