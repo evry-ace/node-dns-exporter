77727072ac8878357b368e775b3c2f1fe4138d90
Hans Kristian Flaatten <hans.flaatten@evry.com>
2021-03-01 20:23:01 +0100

    Handle errors properly

--- README.md
+++ README.md
@@ -5,9 +5,22 @@ DaemonSet in your Kubernetes cluster to report DNS client metrics from each node
 
 ## Features
 
-* [x] resolvconf metrics (`nameserver` and `searchdomain`)
+* [x] resolvconf metrics (`node_dns_nameserver` and `node_dns_searchdomain`)
+* [x] resolv a set of sample domain names evry x seconds to test that name
 
-## ToDo
+## Example Metrics
 
-* [ ] resolv a set of sample domain names evry x minutes to test that name
-  resolution works for the particular node.
+```
+# HELP node_dns_nameserver
+# TYPE node_dns_nameserver counter
+node_dns_nameserver{server="1.1.1.1"} 1
+node_dns_nameserver{server="192.168.40.44"} 1
+# HELP node_dns_test_result
+# TYPE node_dns_test_result gauge
+node_dns_test_result{host="example.com",result="",status="failed"} 0
+node_dns_test_result{host="example.com",result="2606:2800:220:1:248:1893:25c8:1946,93.184.216.34",status="success"} 1
+node_dns_test_result{host="nrk.no",result="",status="failed"} 0
+node_dns_test_result{host="nrk.no",result="23.36.77.90,23.36.77.99,2a02:26f0:4300::1724:4cf1,2a02:26f0:4300::1724:4cf8",status="success"} 1
+node_dns_test_result{host="vg.no",result="",status="failed"} 0
+node_dns_test_result{host="vg.no",result="195.88.54.16,195.88.55.16,2001:67c:21e0::16",status="success"} 1
+```
--- main.go
+++ main.go
@@ -40,8 +40,8 @@ var (
 	)
 )
 
-var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
-var test = flag.String("test-hosts", "bt.no,vg.no,example.com", "Comma separated list of hosts to test DNS resolution")
+var addr = flag.String("listen-address", "127.0.0.1:8080", "The address to listen on for HTTP requests.")
+var test = flag.String("test-hosts", "nrk.no,vg.no,example.com", "Comma separated list of hosts to test DNS resolution")
 var testInterval = flag.Int("test-interval-seconds", 10, "Interval in seconds for running test DNS resolution")
 
 func init() {
@@ -53,31 +53,53 @@ func init() {
 func main() {
 	flag.Parse()
 
-	conf, _ := resolvconf.Get()
-	nameservers := resolvconf.GetNameservers(conf.Content, types.IPv4)
-	searchdomains := resolvconf.GetSearchDomains(conf.Content)
-	testhosts := strings.Split(*test, ",")
+	conf, err := resolvconf.Get()
+	if err != nil {
+		panic(err)
+	}
 
+	nameservers := resolvconf.GetNameservers(conf.Content, types.IPv4)
 	for _, server := range nameservers {
 		promNameserver.With(prometheus.Labels{"server": server}).Inc()
 	}
 
+	searchdomains := resolvconf.GetSearchDomains(conf.Content)
 	for _, host := range searchdomains {
 		promSearchdomain.With(prometheus.Labels{"host": host}).Inc()
 	}
 
+	testhosts := strings.Split(*test, ",")
 	for _, host := range testhosts {
 		ticker := time.NewTicker(time.Duration(float64(*testInterval)) * time.Second)
 		quit := make(chan struct{})
 		go func(host string, metric *prometheus.GaugeVec) {
+			resultPrev := ""
+
 			for {
 				select {
 				case <-ticker.C:
-					result, _ := net.LookupHost(host)
-					sort.Strings(result)
-					fmt.Printf("Resolved %s to %s\n", host, result)
-					metric.With(prometheus.Labels{"host": host, "status": "success", "result": strings.Join(result, ",")}).Set(1)
-					metric.With(prometheus.Labels{"host": host, "status": "failed", "result": ""}).Set(0)
+					result, err := net.LookupHost(host)
+
+					if err != nil {
+						metric.With(prometheus.Labels{"host": host, "status": "success", "result": resultPrev}).Set(0)
+						metric.With(prometheus.Labels{"host": host, "status": "failed", "result": err.Error()}).Set(1)
+
+					} else {
+						sort.Strings(result)
+						fmt.Printf("Resolved %s to %s\n", host, result)
+
+						resultString := strings.Join(result, ",")
+
+						metric.With(prometheus.Labels{"host": host, "status": "success", "result": resultString}).Set(1)
+						metric.With(prometheus.Labels{"host": host, "status": "failed", "result": ""}).Set(0)
+
+						// Reset previous result if it has changed
+						if resultPrev != "" && resultString != resultPrev {
+							metric.With(prometheus.Labels{"host": host, "status": "success", "result": resultPrev}).Set(0)
+						}
+
+						resultPrev = resultString
+					}
 				case <-quit:
 					ticker.Stop()
 					return
@@ -87,5 +109,5 @@ func main() {
 	}
 
 	http.Handle("/metrics", promhttp.Handler())
-	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
